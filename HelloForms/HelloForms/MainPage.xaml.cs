69e9f5124ec13ce7ed5e02bf5f7e557c96cba1d1
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-01-07 09:35:06 -0600

    Changes to benchmark bindings

--- HelloForms/HelloForms/MainPage.xaml.cs
+++ HelloForms/HelloForms/MainPage.xaml.cs
@@ -10,9 +10,35 @@ namespace HelloForms
 {
 	public partial class MainPage : ContentPage
 	{
+		class ViewModel : INotifyPropertyChanged
+		{
+			string text;
+
+			public string Text
+			{
+				get => text;
+				set
+				{
+					text = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+				}
+			}
+
+			public event PropertyChangedEventHandler PropertyChanged;
+		}
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			var vm = new ViewModel { Text = "FooBar" };
+			for (int i = 0; i < 100; i++)
+			{
+				var label = new Label();
+				label.BindingContext = vm;
+				label.SetBinding(Label.TextProperty, "Text");
+				MyLayout.Children.Add(label);
+			}
 		}
 	}
 }

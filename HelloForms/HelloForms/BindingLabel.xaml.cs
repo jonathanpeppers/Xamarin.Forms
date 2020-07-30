74be354b0142629d54f8df544c4419de68354b22
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-07-30 11:36:19 -0500

    Layouts startup test

---
+++ HelloForms/HelloForms/BindingLabel.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloForms
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BindingLabel : Label
+	{
+		public BindingLabel()
+		{
+			InitializeComponent();
+
+			BindingContext = new { Text = "Hello" };
+		}
+	}
+}
--- HelloForms/HelloForms/MainPage.xaml.cs
+++ HelloForms/HelloForms/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace HelloForms
 {
@@ -13,6 +7,13 @@ namespace HelloForms
 		public MainPage()
 		{
 			InitializeComponent();
+
+			var layout = new StackLayout();
+			for (int i = 0; i < 100; i++)
+			{
+				layout.Children.Add(new BindingLabel());
+			}
+			Content = layout;
 		}
 	}
 }
